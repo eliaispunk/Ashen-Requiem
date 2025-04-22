7a5bcb0d0986897b267717fd5c09a6dae12cd94f
eliaispunk <eliateo.sandell@gmail.com>
2025-04-22 14:42:12 +0300

    Background and Sprite

--- ashen-requiem/main_character.gd
+++ ashen-requiem/main_character.gd
@@ -3,12 +3,19 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-
+@onready var sprite_2d: AnimatedSprite2D = $Sprite2D
 
 func _physics_process(delta: float) -> void:
+	# Animations here
+	if (velocity.x > 1 || velocity.x > -1):
+		sprite_2d.animation = "run"
+	else:
+		sprite_2d.animation = "default"
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		sprite_2d.animation = "jump"
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -22,4 +29,7 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+	var isLeft = velocity.x < 0
+	sprite_2d.flip_h = isLeft
+	
 	move_and_slide()

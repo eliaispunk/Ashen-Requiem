ca25f80eb4726dae2de50cecc9c7662980e37a0d
jekkaxi <jessica.kautto@student.kpedu.fi>
2025-04-23 13:17:26 +0300

    Virheellinen

--- ashen-requiem/scenes/character_body_2d_2.gd
+++ ashen-requiem/scenes/character_body_2d_2.gd
@@ -1,52 +1,62 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -550.0
+const GRAVITY = 1200.0
 @onready var sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 var is_attacking: bool = false
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Add gravity
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-		
+		if velocity.y > 0:
+			velocity.y += GRAVITY * 1.3 * delta  # faster falling
+		else:
+			velocity.y += GRAVITY * delta
+
 	# Handle attack input
 	if Input.is_action_just_pressed("attack") and is_on_floor() and not is_attacking:
 		is_attacking = true
+		velocity.x = 0  # Stop horizontal movement immediately
 		sprite_2d.play("attacking")
-		return  # Skip movement while attacking
+		return  # Skip further input this frame
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * SPEED
-		# Flip sprite depending on direction
-		sprite_2d.flip_h = direction < 0
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
-	# Select animations based on state
+	# Skip input while attacking (but still let gravity & move_and_slide run)
 	if not is_attacking:
+		# Handle jump
+		if Input.is_action_just_pressed("jump") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+
+		# Handle movement
+		var direction := Input.get_axis("left", "right")
+		if direction:
+			velocity.x = direction * SPEED
+			sprite_2d.flip_h = direction < 0
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+
+		# Handle animations
 		if not is_on_floor():
-			sprite_2d.play("jumping")
+			if velocity.y < 0:
+				sprite_2d.play("jumping")
+			else:
+				sprite_2d.play("falling")
 		elif direction != 0:
 			sprite_2d.play("running")
 		else:
 			sprite_2d.play("default")
 
 	move_and_slide()
-	
+
+# This gets called when the attack animation finishes
 func _on_Sprite2D_animation_finished():
 	if sprite_2d.animation == "attacking":
 		is_attacking = false
 		if not is_on_floor():
-			sprite_2d.play("jumping")
+			if velocity.y < 0:
+				sprite_2d.play("jumping")
+			else:
+				sprite_2d.play("falling")
 		elif Input.get_axis("left", "right") != 0:
 			sprite_2d.play("running")
 		else:
